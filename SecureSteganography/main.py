6ae4c3788a357f90d00a3234b425bb7f539c03bf
MomenAbuMathana <M.AbuMathana@gju.edu.jo>
2026-05-29 20:58:54 +0300

    Update main.py

--- SecureSteganography/main.py
+++ SecureSteganography/main.py
@@ -1,3 +1,17 @@
-#Version 1.0
-#this class is responsible for making the connection between all classes and managing the flow of the program
+# Version 1.0
+# This class is responsible for making the connection between all classes
+# and managing the flow of the program.
+
+# Import the Interface class from the ui folder.
 from ui.interface import Interface
+
+
+# This condition means:
+# Run the following code only when this file is executed directly.
+if __name__ == "__main__":
+
+    # Create an object from the Interface class.
+    app = Interface()
+
+    # Start the GUI application.
+    app.run()
